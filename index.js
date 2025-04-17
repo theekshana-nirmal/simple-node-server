a4b07ea8764aa3ad1d0c3abde87d4f4d57443411
Theekshana Nirmal <theekshanab1234@gmail.com>
2025-04-17 10:57:58 +0530

    Add HTML files for home and about pages and update server routing

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.end("<h1>Home Page</h1>");
-  } else if (req.url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.end("<h1>About Page</h1>");
-  } else if (req.url === "/contact") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.end("<h1>Contact Us Page</h1>");
-  } else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.end("<h1>Page not found!</h1>");
+// Get the current file name and directory name
+const _fileName = url.fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_fileName);
+
+console.log(_fileName, _dirname)
+
+const server = http.createServer(async (req, res) => {
+  try {
+    if (req.method === "GET") {
+      let filePath;
+      if (req.url === "/") {
+        filePath = path.join(_dirname, "public", "index.html");
+      } else if (req.url === "/about") {
+        filePath = path.join(_dirname, "public", "about.html");
+      } else {
+        throw new Error("Page not found");
+      }
+      const data = await fs.readFile(filePath);
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(data);
+      res.end()
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "content-type": "text/html" });
+    res.end("<h1>Server Error</h1>");
   }
 });
 
